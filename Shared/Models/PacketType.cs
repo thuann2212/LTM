fe71859e411bea23a9980ab485b242737580f87a
TranGiaThuan <Thuan20191207@gmail.com>
2026-05-12 23:21:02 +0700

    Add PacketType class with predefined constants for message types

---
+++ Shared/Models/PacketType.cs
@@ -0,0 +1,12 @@
+namespace Shared.Models
+{
+    public static class PacketType
+    {
+        public const string LOGIN = "LOGIN";
+        public const string CHAT = "CHAT";
+        public const string LOCK_PC = "LOCK_PC";
+        public const string UNLOCK_PC = "UNLOCK_PC";
+        public const string NOTIFICATION = "NOTIFICATION";
+        public const string TIMER_UPDATE = "TIMER_UPDATE";
+    }
+}
